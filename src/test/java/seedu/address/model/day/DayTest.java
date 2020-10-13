3456e6c0331283956ba51461df545f4911760ee5
jiaax <ngjiaxin11@gmail.com>
2020-10-13 23:36:03 +0800

    Added tests for Input and TotalCalories

--- src/test/java/seedu/address/model/day/DayTest.java
+++ src/test/java/seedu/address/model/day/DayTest.java
@@ -1,7 +1,6 @@
 package seedu.address.model.day;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_2;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_2;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_2;
@@ -13,9 +12,18 @@ import static seedu.address.testutil.TypicalDays.BOB;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.day.calorie.*;
 import seedu.address.testutil.DayBuilder;
+import java.util.ArrayList;
 
 public class DayTest {
+
+    public static final Time TIME = new Time("1200");
+    public static final Exercise EXERCISE = new Exercise("run");
+    public static final Food FOOD = new Food("Laksa");
+    public static final CalorieCount CALORIE_COUNT = new CalorieCount("300");
+    public static final Input INPUT = new Input(TIME, FOOD, CALORIE_COUNT);
+    public static final Output OUTPUT = new Output(TIME, EXERCISE, CALORIE_COUNT);
 
     @Test
     public void asObservableList_modifyList_throwsUnsupportedOperationException() {
@@ -91,5 +99,68 @@ public class DayTest {
         // different tags -> returns false
         editedAlice = new DayBuilder(ALICE).withTags(VALID_TAG_HUSBAND).build();
         assertFalse(ALICE.equals(editedAlice));
+    }
+
+    @Test
+    void addTotalCalorieOut() {
+        Day day = new DayBuilder().build();
+        day.addTotalCalorieOut(CALORIE_COUNT);
+        day.addTotalCalorieOut(CALORIE_COUNT);
+        assertEquals(day.getTotalOutputCalorie(), 600);
+    }
+
+    @Test
+    void getCalorieOutputList() {
+        Day day = new DayBuilder().build();
+
+        assertEquals(day.getCalorieOutputList(), new ArrayList<Output>());
+        assertTrue(day.getCalorieOutputList().isEmpty());
+
+        day.addCalorieOutput(OUTPUT);
+        assertFalse(day.getCalorieOutputList().isEmpty());
+    }
+
+    @Test
+    void addCalorieOutput() {
+        Day day = new DayBuilder().build();
+        day.addCalorieOutput(OUTPUT);
+        day.addCalorieOutput(OUTPUT);
+        assertEquals(day.getTotalOutputCalorie(), 600);
+        assertFalse(day.getCalorieOutputList().isEmpty());
+    }
+
+    @Test
+    void getTotalOutputCalorie() {
+        Day day = new DayBuilder().build();
+        day.addCalorieOutput(OUTPUT);
+        day.addCalorieOutput(OUTPUT);
+        assertEquals(day.getTotalOutputCalorie(), 600);
+    }
+
+    @Test
+    void getCalorieInputList() {
+        Day day = new DayBuilder().build();
+        assertEquals(day.getCalorieInputList(), new ArrayList<Input>());
+        assertTrue(day.getCalorieInputList().isEmpty());
+
+        day.addCalorieInput(INPUT);
+        assertFalse(day.getCalorieInputList().isEmpty());
+    }
+
+    @Test
+    void addTotalCalorieInput() {
+        Day day = new DayBuilder().build();
+        day.addTotalCalorieInput(CALORIE_COUNT);
+        day.addTotalCalorieInput(CALORIE_COUNT);
+        assertEquals(day.getTotalInputCalorie(), 600);
+    }
+
+    @Test
+    void addCalorieInput() {
+        Day day = new DayBuilder().build();
+        day.addCalorieInput(INPUT);
+        day.addCalorieInput(INPUT);
+        assertEquals(day.getTotalInputCalorie(), 600);
+        assertFalse(day.getCalorieInputList().isEmpty());
     }
 }
