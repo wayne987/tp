97016802ba02bb9854519c73e01517147ecd3af3
Wayne <>
2020-10-24 23:43:31 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/parser/CalorieCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CalorieCommandParser.java
@@ -72,6 +72,10 @@ public class CalorieCommandParser implements Parser<CalorieCommand> {
         return new CalorieCommand(calorie, type, date);
     }
 
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
